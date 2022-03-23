f47664198b7f66b383295c428c42d99a0c90bac6
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-03-23 11:57:08 +0800

    constructor and addTodo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from "react";
 import "./styles.css";
 
+const Todo = (props) => {
+  return (
+    <li class="todo">
+      <input type="checkbox" />
+      <button>delete</button>
+      <span>{props.todo.text}</span>
+    </li>
+  );
+};
+
 export default class App extends React.Component {
+  //for construct
+  constructor() {
+    super();
+    //declare state
+    this.state = {
+      todos: []
+    };
+  }
+
+  //for function addTodo
+  addTodo() {
+    const text = prompt("TODO text please!");
+    //set the state
+    this.setState({
+      todos: [...this.state.todos, { text: text }]
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <button onClick={() => this.addTodo()}>Add TODO</button>
+        <ul>
+          {this.state.todos.map((todo) => (
+            <Todo todo={todo} />
+          ))}
+        </ul>
       </div>
     );
   }
